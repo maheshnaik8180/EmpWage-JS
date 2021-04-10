cfb207c3a00e561673e8068709690c8f319693bc
GENIUS <naikmahesh.naik@gmail.com>
2021-04-11 01:41:02 +0530

    Calculating wages till a condition is reached for a month

--- employeeWage.js
+++ employeeWage.js
@@ -3,9 +3,10 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 2;
-let empHrs = 0;
-
+const NUM_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 100;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
 function getWorkingHours(empCheck){
 	switch(empCheck){
@@ -17,9 +18,10 @@ function getWorkingHours(empCheck){
       	return 0;
 	}
 }
-for (let day =0; day < NUM_OF_WORKING_DAYS; day++){
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+	totalWorkingDays++;
+   let empCheck = Math.floor(Math.random() * 10) % 3;
+   totalEmpHrs = getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total hours : " +empHrs+ " Emp Wage : " +empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days : " +totalWorkingDays + " Total hours : " +totalEmpHrs+ " Emp Wage : " +empWage);
